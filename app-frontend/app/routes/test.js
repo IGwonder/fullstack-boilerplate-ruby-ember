5f2397e263383cd55715b2ea05e489362e5ffca9
IGwonder <77521056+IGwonder@users.noreply.github.com>
2023-05-11 14:13:14 +0200

    testing each helper

--- app-frontend/app/routes/test.js
+++ app-frontend/app/routes/test.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class TestRoute extends Route {}
+export default class TestRoute extends Route {
+model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
+}
